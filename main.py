7129ed7d8b7a62d44df12afe0fd8290befb831bd
kirin765 <asipalseri@gmail.com>
2025-07-03 21:15:32 +0900

    sec

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def download():
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
         }],
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'geo_bypass': True,
     }
 
     try:
